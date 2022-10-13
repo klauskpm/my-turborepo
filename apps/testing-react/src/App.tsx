3c21213d982975b72d1f52cc95bbb1e3389bac54
Klaus Kazlauskas <klaus.kazlauskas@kazoohr.com>
2022-10-13 08:07:25 -0400

    feat(testing-react): add material ui

--- apps/testing-react/src/App.tsx
+++ apps/testing-react/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import './App.css';
 
 import { Login } from './routes/Login';
--- apps/testing-react/src/index.tsx
+++ apps/testing-react/src/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { CssBaseline } from '@mui/material';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,6 +16,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 );
