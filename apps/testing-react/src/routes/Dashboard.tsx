5b34c0756555f901182075394350bbe9572b9d3b
Klaus Kazlauskas <klaus.kazlauskas@kazoohr.com>
2022-10-16 13:58:42 -0400

    feat(testing-react): create SurveysList component

--- apps/testing-react/src/routes/Dashboard.tsx
+++ apps/testing-react/src/routes/Dashboard.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Typography } from '@mui/material';
 import { GoalsList } from '../ui/GoalsList';
+import { SurveysList } from '../ui/SurveysList';
 
 export function Dashboard() {
   return (
     <>
       <Typography variant='h2'>Dashboard</Typography>
       <GoalsList />
+      <SurveysList />
     </>
   )
 }
---
+++ apps/testing-react/src/ui/SurveysList.tsx
@@ -0,0 +1,9 @@
+import { Paper, Typography } from '@mui/material';
+
+export function SurveysList() {
+  return (
+    <Paper elevation={2}>
+      <Typography variant='body1'>Surveys List</Typography>
+    </Paper>
+  )
+}
