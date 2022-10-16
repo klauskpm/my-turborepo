30175fbe785613d7eeeff7cc7b1fa57a61a9f710
Klaus Kazlauskas <klaus.kazlauskas@kazoohr.com>
2022-10-16 16:45:50 -0400

    feat(testing-react): use Stack on Dashboard

--- apps/testing-react/src/routes/Dashboard.tsx
+++ apps/testing-react/src/routes/Dashboard.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Stack } from "@mui/material";
 import { GoalsList } from '../ui/GoalsList';
 import { SurveysList } from '../ui/SurveysList';
 
 export function Dashboard() {
   return (
     <>
-      <Typography variant='h2'>Dashboard</Typography>
-      <GoalsList />
-      <SurveysList />
+      <Stack spacing={2}>
+        <GoalsList />
+        <SurveysList />
+      </Stack>
     </>
   )
 }
