7d794b020138141650479f1383e0087a9d4fd74e
Klaus Kazlauskas <klaus.kazlauskas@kazoohr.com>
2022-10-16 14:40:34 -0400

    feat(testing-react): create base for Goals and GoalsList

--- apps/testing-react/src/routes/Goals.tsx
+++ apps/testing-react/src/routes/Goals.tsx
@@ -1,8 +1,72 @@
-import React from 'react';
-import { Typography } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  Checkbox,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography
+} from '@mui/material';
+import CommentIcon from '@mui/icons-material/Comment';
+
+const styles = {
+  list: {
+    width: '100%',
+    backgroundColor: 'background.paper'
+  }
+};
 
 export function Goals() {
+  const [checked, setChecked] = useState([0]);
+
+  const handleToggle = (value: number) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
   return (
-    <Typography variant='h2'>Goals</Typography>
+    <>
+      <Typography variant='h3'>Goals</Typography>
+      <List sx={styles.list}>
+        {[0, 1, 2, 3].map((value) => {
+          const labelId = `checkbox-list-label-${value}`;
+
+          return (
+            <ListItem
+              key={value}
+              secondaryAction={
+                <IconButton edge="end" aria-label="comments">
+                  <CommentIcon />
+                </IconButton>
+              }
+              disablePadding
+            >
+              <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
+                <ListItemIcon>
+                  <Checkbox
+                    edge="start"
+                    checked={checked.indexOf(value) !== -1}
+                    tabIndex={-1}
+                    disableRipple
+                    inputProps={{ 'aria-labelledby': labelId }}
+                  />
+                </ListItemIcon>
+                <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+              </ListItemButton>
+            </ListItem>
+          );
+        })}
+      </List>
+    </>
   )
 }
--- apps/testing-react/src/ui/GoalsList.tsx
+++ apps/testing-react/src/ui/GoalsList.tsx
@@ -1,9 +1,40 @@
-import { Paper, Typography } from '@mui/material';
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Paper, Typography
+} from '@mui/material';
+
+const styles = {
+  container: {
+    padding: '16px 24px',
+    width: '100%'
+  },
+  list: {
+    width: '100%',
+    backgroundColor: 'background.paper'
+  }
+};
 
 export function GoalsList() {
+
   return (
-    <Paper elevation={2}>
-      <Typography variant='body1'>Goals List</Typography>
+    <Paper elevation={2} sx={styles.container}>
+      <Typography variant='h5'>Your goals</Typography>
+      <List sx={styles.list}>
+        {[0, 1, 2, 3].map((value) => {
+          const labelId = `checkbox-list-label-${value}`;
+
+          return (
+            <ListItem key={value} disablePadding>
+              <ListItemButton role={undefined} dense>
+                <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+              </ListItemButton>
+            </ListItem>
+          );
+        })}
+      </List>
     </Paper>
   )
 }
