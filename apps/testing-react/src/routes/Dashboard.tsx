e7779bac24db74aecaf9b23f3be4f09c9cffd94b
Klaus Kazlauskas <klaus.kazlauskas@kazoohr.com>
2022-10-16 12:08:34 -0400

    feat(testing-react): create sidebar

--- apps/testing-react/src/routes/Dashboard.tsx
+++ apps/testing-react/src/routes/Dashboard.tsx
@@ -1,4 +1,16 @@
-import { Grid, Paper, Typography } from "@mui/material";
+import { Mail, MoveToInbox } from '@mui/icons-material';
+import {
+  Box,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Paper,
+  Typography
+} from '@mui/material';
+import { NavLink } from 'react-router-dom';
 
 const styles = {
   container: {
@@ -12,6 +24,9 @@ const styles = {
     display: 'flex',
     flexDirection: 'column',
     height: 'fit-content'
+  },
+  active: {
+    color: 'red'
   }
 };
 
@@ -20,6 +35,24 @@ function Dashboard() {
     <Grid container justifyContent='center' sx={styles.container}>
       <Paper sx={styles.paper} elevation={2}>
         <Typography variant='h3'>Dashboard</Typography>
+        <Box>
+          <List>
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <NavLink to={`#${text}`}>
+                  {({isActive}) => (
+                    <ListItemButton sx={isActive ? styles.active : null}>
+                      <ListItemIcon>
+                        {index % 2 === 0 ? <MoveToInbox /> : <Mail />}
+                      </ListItemIcon>
+                      <ListItemText primary={text} />
+                    </ListItemButton>
+                  )}
+                </NavLink>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       </Paper>
     </Grid>
   )
