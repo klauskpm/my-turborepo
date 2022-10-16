448c97f58d11209ad77e7cd5ea970d944bc70e02
Klaus Kazlauskas <klaus.kazlauskas@kazoohr.com>
2022-10-16 11:08:40 -0400

    feat(testing-react): adjust login page styles

--- apps/testing-react/src/routes/Login.tsx
+++ apps/testing-react/src/routes/Login.tsx
@@ -1,22 +1,33 @@
-import { Button, Grid, Paper, TextField, Typography } from "@mui/material";
+import { Button, Grid, Paper, TextField, Typography, Box } from "@mui/material";
 
 const styles = {
+  container: {
+    height: '100%',
+    alignItems: 'center',
+    background: '#C3D4FF'
+  },
   paper: {
-    width: '600px',
+    width: '450px',
     padding: '12px 16px',
     display: 'flex',
-    flexDirection: 'column'
+    flexDirection: 'column',
+    height: 'fit-content'
+  },
+  actions: {
+    marginTop: '16px'
   }
 };
 
 function Login() {
   return (
-    <Grid container justifyContent='center'>
-      <Paper sx={styles.paper}>
+    <Grid container justifyContent='center' sx={styles.container}>
+      <Paper sx={styles.paper} elevation={3}>
         <Typography variant='h4'>Welcome</Typography>
-        <TextField variant='outlined' label='E-mail' autoFocus />
-        <TextField variant='outlined' label='Password' />
-        <Button>Submit</Button>
+        <TextField variant='outlined' label='E-mail' autoFocus margin='normal' />
+        <TextField variant='outlined' label='Password' margin='normal' />
+        <Box sx={styles.actions}>
+          <Button variant='contained'>Submit</Button>
+        </Box>
       </Paper>
     </Grid>
   );
