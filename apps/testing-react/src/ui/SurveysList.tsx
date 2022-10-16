014caf3fa9e792e9fd2e3b200f44ac9ce0596fad
Klaus Kazlauskas <klaus.kazlauskas@kazoohr.com>
2022-10-16 16:46:54 -0400

    feat(testing-react): transform SurveysList into a copy of GoalsList

--- apps/testing-react/src/ui/SurveysList.tsx
+++ apps/testing-react/src/ui/SurveysList.tsx
@@ -1,9 +1,40 @@
-import { Paper, Typography } from '@mui/material';
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Paper, Typography
+} from '@mui/material';
+
+const styles = {
+  container: {
+    padding: '16px 24px',
+    width: '100%'
+  },
+  list: {
+    width: '100%',
+    backgroundColor: 'background.paper'
+  }
+};
 
 export function SurveysList() {
+
   return (
-    <Paper elevation={2}>
-      <Typography variant='body1'>Surveys List</Typography>
+    <Paper elevation={2} sx={styles.container}>
+      <Typography variant='h5'>Your surveys</Typography>
+      <List sx={styles.list}>
+        {[0, 1, 2, 3].map((value) => {
+          const labelId = `checkbox-list-label-${value}`;
+
+          return (
+            <ListItem key={value} disablePadding>
+              <ListItemButton role={undefined} dense>
+                <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+              </ListItemButton>
+            </ListItem>
+          );
+        })}
+      </List>
     </Paper>
   )
 }
