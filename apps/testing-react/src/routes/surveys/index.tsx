a51824b5c6b582df42678c65ab5077290a50e6ba
Klaus Kazlauskas <klaus.kazlauskas@kazoohr.com>
2022-10-16 17:58:33 -0400

    feat(testing-react): add a list of surveys to the Surveys page

--- apps/testing-react/src/routes/surveys/index.tsx
+++ apps/testing-react/src/routes/surveys/index.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
-import { Typography, Stack } from '@mui/material';
+import { Typography, Stack, ListItem, ListItemButton, ListItemText, List } from '@mui/material';
+
+const styles = {
+  list: {
+    width: '100%',
+    backgroundColor: 'background.paper'
+  }
+};
 
 export function Surveys() {
+  const list = [0, 1, 2, 3];
+
   return (
     <Stack spacing={2}>
       <Typography variant='h4'>Your Surveys</Typography>
+      <List sx={styles.list}>
+        {list.map((value) => {
+          const labelId = `checkbox-list-label-${value}`;
+
+          return (
+            <ListItem key={value} disablePadding>
+              <ListItemButton role={undefined} dense>
+                <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+              </ListItemButton>
+            </ListItem>
+          );
+        })}
+      </List>
     </Stack>
   )
 }
