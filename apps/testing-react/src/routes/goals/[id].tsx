cc0a53393d751516e6ea8cb5783384eb6488091c
Klaus Kazlauskas <klaus.kazlauskas@kazoohr.com>
2022-10-16 17:50:04 -0400

    feat(testing-react): add return feat to Goal page

--- apps/testing-react/src/routes/goals/[id].tsx
+++ apps/testing-react/src/routes/goals/[id].tsx
@@ -1,5 +1,24 @@
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Button, Grid, Typography } from "@mui/material";
+import { useNavigate } from 'react-router-dom';
+
 export function Goal() {
+  const navigate = useNavigate();
+
+  const handleReturn = () => {
+    navigate(-1);
+  };
+
   return (
-    <span>View goal</span>
+    <Grid container spacing={2}>
+      <Grid item>
+        <Button variant="outlined" startIcon={<ArrowBackIcon />} onClick={handleReturn}>
+          Return
+        </Button>
+      </Grid>
+      <Grid item>
+        <Typography variant='h5'>Your Goal</Typography>
+      </Grid>
+    </Grid>
   );
 }
