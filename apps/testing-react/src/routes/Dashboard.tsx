9816cc9094698ae64af97676af29f1df9e501628
Klaus Kazlauskas <klaus.kazlauskas@kazoohr.com>
2022-10-16 13:02:25 -0400

    feat(testing-react): create GoalsList component

--- apps/testing-react/src/routes/Dashboard.tsx
+++ apps/testing-react/src/routes/Dashboard.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Typography } from '@mui/material';
+import { GoalsList } from '../ui/GoalsList';
 
 export function Dashboard() {
   return (
-    <Typography variant='h2'>Dashboard</Typography>
+    <>
+      <Typography variant='h2'>Dashboard</Typography>
+      <GoalsList />
+    </>
   )
 }
---
+++ apps/testing-react/src/ui/GoalsList.tsx
@@ -0,0 +1,9 @@
+import { Paper, Typography } from '@mui/material';
+
+export function GoalsList() {
+  return (
+    <Paper elevation={2}>
+      <Typography variant='body1'>Goals List</Typography>
+    </Paper>
+  )
+}
