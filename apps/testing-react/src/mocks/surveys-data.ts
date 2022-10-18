108bbfa0c926e636db1121d99da4b31883f0c996
Klaus Kazlauskas <klaus.kazlauskas@kazoohr.com>
2022-10-18 08:40:38 -0400

    refactor: redo apps

--- apps/testing-react/src/mocks/surveys-data.ts
+++
@@ -1,6 +0,0 @@
-export const surveysData = [
-  { id: 1, title: 'Engagement Survey', answered: true, __typename: 'Survey' },
-  { id: 2, title: 'DEI Survey', answered: false, __typename: 'Survey' },
-  { id: 3, title: 'Exit Survey', answered: false, __typename: 'Survey' },
-  { id: 4, title: 'Onboard Survey', answered: true, __typename: 'Survey' },
-];
--- apps/testing-react/src/ui/Profile.tsx
+++
@@ -1,7 +0,0 @@
-import { Box } from "@mui/material";
-
-export function Profile() {
-  return (
-    <Box>Profile</Box>
-  )
-}
