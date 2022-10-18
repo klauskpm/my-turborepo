c8bfbe6e7e977d0b75449c1decfa027a4dd03971
Klaus Kazlauskas <klaus.kazlauskas@kazoohr.com>
2022-10-17 20:29:28 -0400

    feat(otamatone): add basic HTML to survey details page

--- apps/otamatone/src/routes/surveys/[pid].tsx
+++ apps/otamatone/src/routes/surveys/[pid].tsx
@@ -1,5 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+import { Button, Grid, Typography } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
 export function SurveyDetails() {
+  const navigate = useNavigate();
+
+  const handleReturn = () => {
+    navigate(-1);
+  };
+
   return (
-    <div>Survey Details page</div>
+    <Grid container spacing={2}>
+      <Grid item>
+        <Button variant="outlined" startIcon={<ArrowBackIcon />} onClick={handleReturn}>
+          Return
+        </Button>
+      </Grid>
+      <Grid item>
+        <Typography variant='h5'>Surveys Details</Typography>
+      </Grid>
+    </Grid>
   );
 }
