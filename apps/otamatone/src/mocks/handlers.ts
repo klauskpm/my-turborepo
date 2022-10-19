78b01797edd80c2e8734da9f340e9ae65aa8d84c
Klaus Kazlauskas <klaus.kazlauskas@kazoohr.com>
2022-10-18 22:42:43 -0400

    feat(otamatone): use @shared/util-msw

--- apps/otamatone/src/mocks/handlers.ts
+++ apps/otamatone/src/mocks/handlers.ts
@@ -1,25 +1,7 @@
-import { graphql } from 'msw';
-
-import { surveysData, surveyWithResponses } from "./surveys-data";
-import { goalsData } from './goals-data';
+import { goalsHandlers } from '@goal/data-access';
+import { surveysHandlers } from "@survey/data-access";
 
 export const handlers = [
-  graphql.query('GetSurveys', (req, res, ctx) => {
-    return res(ctx.data({ surveys: surveysData }));
-  }),
-  graphql.query('GetUnansweredSurveys', (req, res, ctx) => {
-    const surveys = surveysData.filter(((survey) => !survey.answered));
-    return res(ctx.data({ surveys }));
-  }),
-  graphql.query('GetSurveyDetails', (req, res, ctx) => {
-    return res(ctx.data({ survey: surveyWithResponses }));
-  }),
-
-  graphql.query('GetGoals', (req, res, ctx) => {
-    return res(ctx.data({ goals: goalsData}));
-  }),
-  graphql.query('GetGoalsTodo', (req, res, ctx) => {
-    const goals = goalsData.filter(((goal) => goal.done));
-    return res(ctx.data({ goals }));
-  })
+  ...surveysHandlers,
+  ...goalsHandlers
 ];
